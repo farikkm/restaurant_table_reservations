18543ba2081899e7549441aebd5623bf146d405b
ofi-piko <tamponpon795@gmail.com>
2026-07-06 18:58:28 +0500

    Я что-то сделал

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,9 +1,30 @@
+from pathlib import Path
+
 from aiogram import Router
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.types import Message, FSInputFile, InlineKeyboardMarkup, InlineKeyboardButton
 
 router = Router()
+BASE_DIR = Path(__file__).resolve().parent.parent
+photo_path = BASE_DIR / "img" / "images.png"
 
-@router.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer("Привет, я бот для бронирования и записей!")
+@router.message(Command("start"))
+async def start(message: Message):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="🍽 Забронировать столик", callback_data="btn1")],
+            [InlineKeyboardButton(text="📋 Мои бронирования", callback_data="btn2")],
+            [InlineKeyboardButton(text="ℹ️ О ресторане", callback_data="btn3")]
+        ]
+    )
+
+    if photo_path.exists():
+        photo = FSInputFile(str(photo_path))
+        await message.answer_photo(
+            photo=photo,
+            reply_markup=keyboard
+        )
+    else:
+        await message.answer(
+            reply_markup=keyboard
+        )
