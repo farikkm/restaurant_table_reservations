669d8e9821c767863865a1a90a3bccca8e2a16f8
Fariz <140306287+farikkm@users.noreply.github.com>
2026-07-06 16:17:34 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import logging
+from app.utils.logger_config import setup_logger
+
+from os import getenv
+from dotenv import load_dotenv
+
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from app.handlers.start import router as start_router
+from app.handlers.custom import router as custom_router
+
+setup_logger()
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+TOKEN = getenv("BOT_TOKEN")
+
+dp = Dispatcher()
+dp.include_router(start_router)
+dp.include_router(custom_router)
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
